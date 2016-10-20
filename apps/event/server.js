7b31a17e2ecbb05f95539fb3439781515cbb8b21
Charles Broskoski <broskoski@gmail.com>
2016-10-20 10:33:40 -0400

    Add reservation API

--- apps/event/server.js
+++ apps/event/server.js
@@ -2,12 +2,12 @@ import Koa from 'koa'
 import { graphqlize } from 'joiql-mongo'
 import router from './router'
 import * as event from './models/event'
-// import * as reservation from './models/reservation'
+import * as reservation from './models/reservation'
 
 const app = new Koa()
 
 router.all('/api/event', graphqlize(event))
-// router.all('/api/reservation', graphqlize(reservation))
+router.all('/api/reservation', graphqlize(reservation))
 app.use(router.routes())
 
 export default app
