794c8142e2f017cbfbd43547ae7beebd2005356b
Charles Broskoski <broskoski@gmail.com>
2016-10-25 15:26:04 -0400

    Adds is_waitlisted middleware

--- apps/event/models/reservation/index.js
+++ apps/event/models/reservation/index.js
@@ -1,6 +1,8 @@
-import { model, string, array, objectid, date, number } from 'joiql-mongo'
+import { model, string, array, objectid, date, number, boolean, db } from 'joiql-mongo'
+import { assign } from 'lodash'
+import event from '../event/index.js'
 
-export default model('reservation', {
+const reservation = model('reservation', {
   name: string(),
   email: string(),
   guests: array().items(string()),
@@ -9,4 +11,27 @@ export default model('reservation', {
     .meta((is) => ({
       'create': is.forbidden().default(new Date())
     })),
+  is_waitlisted: boolean()
+    .meta((is, something) => ({
+      'delete create': is.forbidden()
+    }))
 })
+
+const isReservationWaitlisted = async (reservation) => {
+  const reservationEvent = await event.find({ _id: `${reservation.event_id}` })
+  const reservationCount = await db.reservations.count({ event_id: `${reservation.event_id}` })
+  return reservationEvent.capacity <= reservationCount
+}
+
+const isWaitlistedMiddleware = async (ctx, next) => {
+  await next()
+  const is_waitlisted = await isReservationWaitlisted(ctx.res.createReservation)
+  await reservation.update(assign(ctx.res.createReservation, {
+     _id: `${ctx.res.createReservation._id}`,
+    is_waitlisted: is_waitlisted
+  }))
+}
+
+reservation.on('create', isWaitlistedMiddleware)
+
+export default reservation
