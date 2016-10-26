da40af46c1e2bdcf0407815430c0abe6994436ad
Charles Broskoski <broskoski@gmail.com>
2016-10-26 13:53:36 -0400

    Adds one sad test

--- test/views/admin/event_form.js
+++ test/views/admin/event_form.js
@@ -1,15 +1,22 @@
 /* eslint-env mocha */
-import { fixture, render } from '../helpers'
-import EventForm from '../../../admin/views/event_form'
+import Baobab from 'baobab'
+import { fixture, render } from '../../helpers'
+import EventForm from '../../../apps/admin/views/event_form'
 
 describe('EventForm', () => {
+  let event
+
+  beforeEach(() => {
+    event = new Baobab({ event: fixture('event') })
+  })
+
   it('renders form for the event', async () => {
     const props = {
-      event: fixture('event'),
+      event: event,
       onSubmit: () => {},
       buttonText: 'Create Event',
       error: ''
     }
-    render(EventForm, props).html().should.containEql('Orta')
+    render(EventForm, props).html().should.containEql('Create Event')
   })
 })
