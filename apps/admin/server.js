9f94574c51175c5294636b2b1fd84aeca74d1e3c
Charles Broskoski <broskoski@gmail.com>
2016-10-26 22:45:32 -0400

    Starts basic auth, not working

--- apps/admin/server.js
+++ apps/admin/server.js
@@ -1,8 +1,22 @@
 import Koa from 'koa'
 import router from './router'
+import mount from 'koa-mount'
+import auth from 'koa-basic-auth'
 
 const app = new Koa()
 
+// const {
+//   BASIC_AUTH_USERNAME,
+//   BASIC_AUTH_PASSWORD
+// } = process.env
+
+// app.use(function (ctx, next){
+//   ctx.status = 401
+//   ctx.set('WWW-Authenticate', 'Basic realm=Authorization Required')
+//   return next()
+// });
+
+// app.use(mount('/admin', auth({ name: BASIC_AUTH_USERNAME, pass: BASIC_AUTH_PASSWORD })))
 app.use(router.routes())
 
 export default app
