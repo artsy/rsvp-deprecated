ba33732c966696177899c1bdf27596233a85769a
Charles Broskoski <broskoski@gmail.com>
2016-10-28 09:33:46 -0400

    Auth middleware for /admin

--- apps/admin/server.js
+++ apps/admin/server.js
@@ -1,22 +1,30 @@
 import Koa from 'koa'
 import router from './router'
-import mount from 'koa-mount'
 import auth from 'koa-basic-auth'
 
 const app = new Koa()
 
-// const {
-//   BASIC_AUTH_USERNAME,
-//   BASIC_AUTH_PASSWORD
-// } = process.env
+const {
+  BASIC_AUTH_USERNAME,
+  BASIC_AUTH_PASSWORD
+} = process.env
 
-// app.use(function (ctx, next){
-//   ctx.status = 401
-//   ctx.set('WWW-Authenticate', 'Basic realm=Authorization Required')
-//   return next()
-// });
+app.use(async (ctx, next) => {
+  if (ctx.url.indexOf('admin') >= 0) {
+    try {
+      return auth({ name: BASIC_AUTH_USERNAME, pass: BASIC_AUTH_PASSWORD })(ctx, next)
+    } catch (err) {
+      if (err.status === 401) {
+        ctx.status = 401
+        ctx.set('WWW-Authenticate', 'Basic realm=Authorization Required')
+      } else {
+        throw err
+      }
+    }
+  }
+  return next()
+})
 
-// app.use(mount('/admin', auth({ name: BASIC_AUTH_USERNAME, pass: BASIC_AUTH_PASSWORD })))
 app.use(router.routes())
 
 export default app
